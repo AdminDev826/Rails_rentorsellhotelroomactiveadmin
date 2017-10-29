475a0ae6d2df25dd1583b1f88d568ae74ce956fe
admin <admin@admins-Mac.local>
2017-10-29 12:41:00 +0800

    chage mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
-      :user_name            => 'araddi.com@gmail.com',
-      :password             => '@Nadus1979!',
+      :user_name            => 'huawan028@gmail.com',
+      :password             => 'feezuygxkeptrguz',
       :authentication       => 'plain',
       :enable_starttls_auto => true  }
 
